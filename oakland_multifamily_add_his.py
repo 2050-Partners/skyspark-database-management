9794cae60dfb94fc2a160e4ea991c0ac78db272b
Eric Swanson <ericswanson@2050partners.com>
2025-08-11 18:22:35 -0700

    Add script to add history for Oakland multifamily

---
+++ oakland_multifamily_add_his.py
@@ -0,0 +1,81 @@
+'''
+Script to add histories to points in the Oakland multifamily site in the Code Readiness Research project in SkySpark.
+Iterate through a folder of CSV files. Each CSV contains a SkySpark point ID in the filename. Using this ID, add
+the data in the CSV as a history to the point in SkySpark.
+'''
+
+### IMPORT ###
+import os
+import math
+import csv
+import urllib
+import glob
+from zoneinfo import ZoneInfo
+from phable import open_haxall_client, open_haystack_client, DateRange, Ref, Grid, Number, Marker
+from datetime import datetime, date, timedelta, timezone
+from dotenv import load_dotenv
+
+from skyspark_database_funcs import *
+
+### FUNCTION ###
+
+
+
+### MAIN ###  
+print("start:", datetime.now(), "\n")
+
+#load credentials from .env file
+username, password, uri = loadCredentials() #"https://codereadiness.com/api/crResearch"
+
+#csvFolder = "/Users/ericswanson/Library/CloudStorage/OneDrive-2050Partners,Inc/Documents/test-csv" JUST FOR TESTING
+csvFolder = "/Users/ericswanson/Library/CloudStorage/OneDrive-SharedLibraries-2050Partners,Inc/Elise Wall - sensorFiles"
+csvList = glob.glob(csvFolder + '/*.csv')
+
+for i,file in enumerate(csvList):
+
+    #create the Ref from the point ID in the filename
+    filename = file.split('/')[-1]
+    pointId = filename.split('_')[0]
+    #idRef = Ref("p:crResearch:r:" + pointId)
+    idRef = Ref("p:firstThing:r:" + pointId)
+    
+    with open_haystack_client(uri, username, password) as haystack_client:
+
+        #each point may have different units, so need to grab those from SkySpark before creating a grid of data
+        pointUnits = haystack_client.read_by_id(idRef)['unit']
+
+        #Create a mock Haystack grid for the point history from the csv file
+        his = csv2grid(file, units=pointUnits)
+
+        errorCount = 0
+        try:
+            response = haystack_client.his_write_by_id(idRef, his)
+
+        except urllib.error.HTTPError as eHttp:
+            print("HTTP error with", idRef, "full grid. Trying again.")
+
+            #if you get a 500 error back, the grid is mostly likely too big. Try again but with half the grid at a time
+            try:
+                mid = math.ceil(len(his)/2)
+                response = haystack_client.his_write_by_id(idRef, his[0:mid])
+                response = haystack_client.his_write_by_id(idRef, his[mid:-1])
+
+            except Exception as e:
+                print('Failed again for', idRef)
+                errorCount += 1
+
+
+print("Number of errors", errorCount)
+
+print("\nEnd:", datetime.now())
+
+        
+
+
+        
+    
+
+
+
+
+
---
+++ skyspark_database_funcs.py
@@ -0,0 +1,106 @@
+'''
+Python file containing useful functions for SkySpark database management through the API.
+'''
+
+### IMPORT ###
+import os
+import csv
+import pandas
+from zoneinfo import ZoneInfo
+from phable import open_haxall_client, open_haystack_client, DateRange, Ref, Grid, Number, Marker
+from datetime import datetime, date, timedelta, timezone
+from dotenv import load_dotenv
+
+
+
+### ERRORS ###
+class EmptyUnitsError(Exception):
+    '''
+    Error raised when user tries to access a point in SkySpark without specifying units.
+
+    help_msg: A display to help with troubleshooting.
+    '''
+
+    help_msg : str
+
+
+
+
+### FUNCTIONS ###
+def csv2grid(filepath: str, tz=ZoneInfo('America/Los_Angeles'), units="", returnHeaders=False):
+    '''
+    Read a csv file to create a (mock Python version of a) Haystack Grid object. 
+    The csv file should contain 2 columns: 1 column of data to be used for the point value and 1
+    column of the corresponding datetime for that data. There should be 1 header row.
+
+    Inputs:
+        filepath (str) - Filepath to the csv file
+        tz (ZoneInfo Python object) - ZoneInfo Python object specifying the timezone of the datetime data.
+            Optional - default is 'America/Los_Angeles'
+        units (str) - Units corresponding to the data values. 
+        returnHeaders (bool) - If True, functions returns the history grid and the headers from the csv file. If
+            False, function only returns the history grid.
+    Outputs:
+        his (list) - list of dictionary objects. Each dictionary has the same two fields: 'ts' and 'val'. 'val' is
+            data to be used for the point value and 'ts' is the corresponding datetime that data was captured. This list
+            is meant to represent a Haystack Grid object.
+        headers (list) - Optional output. A list of strings corresponding to the headers of the csv file.
+    '''
+
+    if units=="":
+        raise EmptyUnitsError("Point units not specified in function call parameters")
+
+    #create mock Haystack grid structure
+    his = []
+
+    with open(filepath) as csvfile:
+        csvReader = csv.reader(csvfile, delimiter=',')
+
+        # Get the first row as headers. Headers are the key names in the grid dict
+        headers = next(csvReader)  
+        
+        #load in csv into mock grid structure
+        for i,row in enumerate(csvReader):
+
+            #convert the date string from the csv into a timezone-aware datetime object
+            if row[1] == "NA":
+                continue
+            else:
+                ts = datetime.strptime(row[1], '%Y-%m-%dT%H:%M:%SZ')
+                ts = ts.replace(tzinfo = tz)
+
+                #add row of data to the grid. History should only have the keys 'ts' and 'val' (per API)
+                #TODO: more efficiently load in data
+                hisRow = {'ts': ts, 'val': Number(float(row[0]), units)}
+                his.append(hisRow)
+
+    if returnHeaders:
+        return his, headers
+    else:
+        return his
+    
+
+def loadCredentials():
+    '''
+    Loads credentials from a .env file to allow the user to connect to the SkySpark API.
+    .env file must live in the same directory as this file. 
+
+    Inputs:
+        none
+    Returns:
+        username (str) - SkySpark username
+        password (str) - SkySpark password
+        uri (str) - uri to the SkySpark project you'd like to access. For example, https://codereadiness.com/api/firstThing
+
+    Example .env:
+        DB_USERNAME="yourUsername"
+        DB_PASSWORD="yourPasword"
+        DB_URI="https://codereadiness.com/api/firstThing"
+    '''
+
+    load_dotenv()
+    username = os.getenv("DB_USERNAME")
+    password = os.getenv("DB_PASSWORD")
+    uri      = os.getenv("DB_URI")
+
+    return username, password, uri
