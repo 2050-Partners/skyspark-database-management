ebacafb83a5dcfa79fe8a20793a345ea9b5a328b
Eric Swanson <ericswanson@2050partners.com>
2025-08-13 16:53:28 -0700

    test code to add new tags for all sites in a project

--- ESH_add_tags.py
+++ ESH_add_tags.py
@@ -26,8 +26,27 @@ print("start:", datetime.now(), "\n")
 #load credentials from .env file
 username, password, uri = loadCredentials()
 
+#first, get list of all site IDs
 with open_haystack_client(uri, username, password) as haystack_client:
+    meta, data = haystack_client.read_all("site").to_pandas_all()
+    siteIds = data.id.values
 
-    #get list of all sites
-    data, meta = haystack_client.read_all("site")
+#Update the sites with new metadata tags
+with open_haxall_client (uri, username, password) as haxall_client:
+
+    for id in siteIds:
+
+        siteRef = Ref(id.val)
+
+        #query entire rec we want to modify to get the mod tag
+        rec = haxall_client.read_by_id(siteRef)
+
+        #define new tag to add to rec
+        rec["ericTagTest"] = "test tag for each site"
+
+        #commit update to rec and capture response
+        meta, rec_modified_grid = haxall_client.commit_update(rec).to_pandas_all()
+
+
+print("end:", datetime.now())
 
--- oakland_multifamily_add_his.py
+++ oakland_multifamily_add_his.py
@@ -32,66 +32,70 @@ uri = "https://codereadiness.com/api/crResearch"
 #csvFolder = "/Users/ericswanson/Library/CloudStorage/OneDrive-2050Partners,Inc/Documents/test-csv" JUST FOR TESTING
 csvFolder = "/Users/ericswanson/Library/CloudStorage/OneDrive-SharedLibraries-2050Partners,Inc/Elise Wall - sensorFiles"
 csvList = glob.glob(csvFolder + '/*.csv')
+#csvList.sort()
 
 for i,file in enumerate(csvList):
 
-    #pick up where script left off
-    if i > 13:
-        nextFile = False
+    # #pick up where script left off
+    # if i > 13:
+    nextFile = False
 
-        while nextFile == False:
+    while nextFile == False:
 
-            #create the Ref from the point ID in the filename
-            filename = file.split('/')[-1]
-            pointId = filename.split('_')[0]
-            #idRef = Ref("p:firstThing:r:" + pointId) JUST FOR TESTING
-            idRef = Ref("p:crResearch:r:" + pointId)
-            
-            with open_haystack_client(uri, username, password) as haystack_client:
+        #create the Ref from the point ID in the filename
+        filename = file.split('/')[-1]
+        pointId = filename.split('_')[0]
+        #idRef = Ref("p:firstThing:r:" + pointId) JUST FOR TESTING
+        idRef = Ref("p:crResearch:r:" + pointId)
+        
+        with open_haystack_client(uri, username, password) as haystack_client:
 
-                #each point may have different units, so need to grab those from SkySpark before creating a grid of data
-                pointUnits = haystack_client.read_by_id(idRef)['unit']
+            #each point may have different units, so need to grab those from SkySpark before creating a grid of data
+            pointUnits = haystack_client.read_by_id(idRef)['unit']
 
-                #Create a mock Haystack grid for the point history from the csv file
-                his = csv2grid(file, units=pointUnits)
+            #Create a mock Haystack grid for the point history from the csv file
+            his = csv2grid(file, units=pointUnits)
+            mid = math.ceil(len(his)/2)
 
-                errorCount = 0
-                try:
-                    response = haystack_client.his_write_by_id(idRef, his)
-                    nextFile = True
+            errorCount = 0
+            try:
+                response = haystack_client.his_write_by_id(idRef, his)
+                nextFile = True
+                print("successful first attempt for", idRef, "-- i =", i)
 
-                except urllib.error.HTTPError as eHttp:
-                    print("HTTP error with", idRef, "Trying again.")
+            except urllib.error.HTTPError as eHttp:
+                print("HTTP error with", idRef, "Trying again -- i =", i)
 
-                    #if you get a 500 error back, the grid is mostly likely too big. Try again but with half the grid at a time
-                    if eHttp.status == 500:
+                #if you get a 500 error back, the grid is mostly likely too big. Try again but with half the grid at a time
+                if eHttp.status == 500:
 
-                        print("# of rows for point history =", len(his))
+                    print("# of rows for point history =", len(his))
 
-                        try:
-                            mid = math.ceil(len(his)/2)
-                            response = haystack_client.his_write_by_id(idRef, his[0:mid])
-                            response = haystack_client.his_write_by_id(idRef, his[mid:-1])
-                            nextFile = True
+                    try:
+                        response = haystack_client.his_write_by_id(idRef, his[0:mid])
+                        response = haystack_client.his_write_by_id(idRef, his[mid:-1])
+                        nextFile = True
+                        print("successful write for", idRef, "-- i =", i)
 
-                        except Exception as e:
-                            print('Failed again for', idRef)
-                            errorCount += 1
+                    except Exception as e:
+                        print('Failed again for', idRef, " -- i =", i)
+                        errorCount += 1
 
-                    #if you get a 502 error back, there's a server error and it's most likely overloaded. Wait a few minutes and try again.
-                    elif eHttp.status == 502:
+                #if you get a 502 error back, there's a server error and it's most likely overloaded. Wait a few minutes and try again.
+                elif eHttp.status == 502:
 
-                        #wait 5 minutes
-                        time.sleep(300)
+                    #wait 5 minutes
+                    time.sleep(300)
 
-                        try:
-                            response = haystack_client.his_write_by_id(idRef, his)
-                            nextFile = True
+                    try:
+                        response = haystack_client.his_write_by_id(idRef, his)
+                        nextFile = True
+                        print("successful write for", idRef, "-- i =", i)
 
-                        except Exception as e:
-                            print('Failed again for', idRef)
-                            nextFile = True
-                            errorCount += 1
+                    except Exception as e:
+                        print('Failed again for', idRef, " -- i =", i)
+                        nextFile = True
+                        errorCount += 1
 
 
 print("Number of errors", errorCount)
