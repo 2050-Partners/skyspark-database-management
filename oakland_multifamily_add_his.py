e4a8c5ddce57e25b6eb0d02822f1f168d4e40dde
Eric Swanson <ericswanson@2050partners.com>
2025-08-19 14:40:41 -0700

    batch upload multifamily script

--- oakland_multifamily_add_his.py
+++ oakland_multifamily_add_his.py
@@ -12,7 +12,7 @@ import csv
 import urllib
 import glob
 from zoneinfo import ZoneInfo
-from phable import open_haxall_client, open_haystack_client, DateRange, Ref, Grid, Number, Marker
+from phable import open_haxall_client, open_haystack_client, DateRange, Ref, Grid, Number, Marker, CallError
 from datetime import datetime, date, timedelta, timezone
 from dotenv import load_dotenv
 
@@ -25,11 +25,12 @@ from skyspark_database_funcs import *
 ### MAIN ###  
 print("start:", datetime.now(), "\n")
 
+#TODO: add logging
+
 #load credentials from .env file
 username, password, uri = loadCredentials() #"https://codereadiness.com/api/crResearch"
 uri = "https://codereadiness.com/api/crResearch"
 
-#csvFolder = "/Users/ericswanson/Library/CloudStorage/OneDrive-2050Partners,Inc/Documents/test-csv" JUST FOR TESTING
 csvFolder = "/Users/ericswanson/Library/CloudStorage/OneDrive-SharedLibraries-2050Partners,Inc/Elise Wall - sensorFiles"
 csvList = glob.glob(csvFolder + '/*.csv')
 #csvList.sort()
@@ -38,8 +39,12 @@ errorCount = 0
 idFailList = []
 for i,file in enumerate(csvList):
 
-    # #pick up where script left off
-    # if i > 13:
+    #pick up where script left off
+    if i < 150:
+        continue
+
+    #TODO: create a timeout in case script gets stuck on any one interation for too long
+
     nextFile = False
 
     while nextFile == False:
@@ -47,74 +52,108 @@ for i,file in enumerate(csvList):
         #create the Ref from the point ID in the filename
         filename = file.split('/')[-1]
         pointId = filename.split('_')[0]
-        #idRef = Ref("p:firstThing:r:" + pointId) JUST FOR TESTING
         idRef = Ref("p:crResearch:r:" + pointId)
         
         with open_haystack_client(uri, username, password) as haystack_client:
 
             #each point may have different units, so need to grab those from SkySpark before creating a grid of data
-            pointUnits = haystack_client.read_by_id(idRef)['unit']
+            siteMeta = haystack_client.read_by_id(idRef)
+            pointUnits = siteMeta['unit']
+
+            # #for now, skip any points that have a history -- may be causing errors 8/14
+            # if 'hisStart' in siteMeta:
+            #     nextFile = True
+            #     print("point", idRef, "already has history -- skipping file -- i=", i)
+            #     continue
 
             #Create a mock Haystack grid for the point history from the csv file
             his = csv2grid(file, units=pointUnits)
-            mid = math.ceil(len(his)/2)
+            # mid = math.ceil(len(his)/2)
 
+            #split the grid into 1 million row chunks + 1 chunk for leftover rows under 1 million
+            numChunks = math.floor(len(his)/1000000) + 1
+            #upload the history 1 million rows at a time to avoid errors
             try:
-                response = haystack_client.his_write_by_id(idRef, his)
+                for j in range(0, numChunks):
+                    start = j * 1000000
+
+                    #last chunk is the end of the history (Grid)
+                    if j == (numChunks - 1):
+                        end = -1
+
+                    else:
+                        end = (j + 1) * 1000000
+
+                    haystack_client.his_write_by_id(idRef, his[start:end])
+
                 nextFile = True
-                print("successful first attempt for", idRef, "-- i =", i)
+                print("successful upload for", idRef, "-- i =", i)
 
-            # except urllib.error.URLError as eUrl:
-            #     print(f"URL error for", idRef, "-- i =", i)
-            #     test = 0
-
-            # except urllib.error.URLError as e:
             except Exception as e:
-
-                if isinstance(e, urllib.error.URLError):
-                    print(f"{e} error for", idRef, "-- skipping file -- i =", i)
-                    nextFile = True
-                    idFailList.append(idRef)
-
-                elif isinstance(e, urllib.error.HTTPError):
-                    print(f"{e} error for", idRef, "-- Trying again -- i =", i)
-
-                    #if you get a 500 error back, the grid is mostly likely too big. Try again but with half the grid at a time
-                    if e.status == 500:
-
-                        print("# of rows for point history =", len(his))
-
-                        try:
-                            response = haystack_client.his_write_by_id(idRef, his[0:mid])
-                            response = haystack_client.his_write_by_id(idRef, his[mid:-1])
-                            nextFile = True
-                            print("successful write for", idRef, "-- i =", i)
-
-                        except Exception as e:
-                            print('Failed again for', idRef, " -- i =", i)
-                            errorCount += 1
-                            nextFile = True
-                            idFailList.append(idRef)
-
-                    #if you get a 502 error back, there's a server error and it's most likely overloaded. Wait a few minutes and try again.
-                    elif e.status == 502:
-
-                        #wait 5 minutes
-                        time.sleep(300)
-
-                        try:
-                            response = haystack_client.his_write_by_id(idRef, his)
-                            nextFile = True
-                            print("successful write for", idRef, "-- i =", i)
-
-                        except Exception as e:
-                            print('Failed again for', idRef, " -- i =", i)
-                            nextFile = True
-                            errorCount += 1
-                            idFailList.append(idRef)
+                print(f"{e} for", idRef, "-- skipping file -- i =", i)
+                nextFile = True
+                idFailList.append(idRef)
 
 
-print("Number of errors", errorCount)
+
+            # try:
+            #     haystack_client.his_write_by_id(idRef, his[0:1000])
+            #     haystack_client.his_write_by_id(idRef, his[1000:mid])
+            #     haystack_client.his_write_by_id(idRef, his[mid:-1])
+            #     nextFile = True
+            #     print("successful first attempt for", idRef, "-- i =", i)
+
+            # except Exception as e:
+
+            #     if isinstance(e, CallError):
+            #         msg = e.help_msg.meta
+            #         print(msg)
+            #         nextFile = True
+            #         idFailList.append(idRef)
+            #         print('phable error  for', idRef, ' skipping file -- i=', i)
+
+            #     elif isinstance(e, urllib.error.HTTPError):
+            #         print(f"{e} for", idRef, "-- Trying again -- i =", i)
+
+            #         #if you get a 500 error back, the grid is mostly likely too big. Try again but with half the grid at a time
+            #         if e.status == 500:
+
+            #             print("# of rows for point history =", len(his))
+
+            #             try:
+            #                 response = haystack_client.his_write_by_id(idRef, his[0:mid])
+            #                 response = haystack_client.his_write_by_id(idRef, his[mid:-1])
+            #                 nextFile = True
+            #                 print("successful write for", idRef, "-- i =", i)
+
+            #             except Exception as e:
+            #                 print('Failed again for', idRef, " -- i =", i)
+            #                 nextFile = True
+            #                 idFailList.append(idRef)
+
+            #         #if you get a 502 error back, there's a server error and it's most likely overloaded. Wait a few minutes and try again.
+            #         elif e.status == 502:
+
+            #             #wait 5 minutes
+            #             time.sleep(300)
+
+            #             try:
+            #                 response = haystack_client.his_write_by_id(idRef, his)
+            #                 nextFile = True
+            #                 print("successful write for", idRef, "-- i =", i)
+
+            #             except Exception as e:
+            #                 print('Failed again for', idRef, " -- i =", i)
+            #                 nextFile = True
+            #                 idFailList.append(idRef)
+
+            #     elif isinstance(e, urllib.error.URLError):
+            #         print(f"{e} for", idRef, "-- skipping file -- i =", i)
+            #         nextFile = True
+            #         idFailList.append(idRef)
+
+
+print("Number of fails", len(idFailList))
 print("IDs that failed", idFailList)
 
 print("\nEnd:", datetime.now())
